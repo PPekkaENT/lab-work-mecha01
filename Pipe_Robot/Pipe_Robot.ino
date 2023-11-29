f2f663802711dbbd97585f8e77f1f6d2a3e2c066
pyry <pyrypekka.harkonen@gmail.com>
2023-11-29 22:13:49 +0200

    update Pipe_Robot

--- Pipe_Robot/Pipe_Robot.ino
+++ Pipe_Robot/Pipe_Robot.ino
@@ -2,7 +2,7 @@
 #include <Servo.h>
 #include <IRSendRev.h>
 
-// Digital pins [2-6,8]
+// Digital pins [2-5,8,9,11]
 // hall sensor pin/interruptPin
 const int hallPin = 2;
 // LED pin
@@ -10,38 +10,21 @@ const int ledPin = 3;
 // D05 IR receiver
 const int IR_Receiver=5;
 // IR reflective nr.1
-//const int irPin = 4;
-// servo10 = D10
-// BLDC = D08
+const int irPin = 4;
 const int pinI1=8;     // motor 1 pin1
 const int pinI2=11;    //motor 1 pin2
 const int speedpinA=9; //EA(PWM)to control the motor_1 speed
 
 // Variables
-// Servo motors
-Servo servo10;
-// BLDC motor/ESC (Servo object)
-//Servo ESC;
 // array for ir receiver data
 unsigned char dta[20];
 // go command
 bool goCmd = false; 
 // IR reflective sensor states
-//int irState = 0;
-
-struct Robot{
-  int power;
-};
-Robot robot = {0};
-
-// Define protypes here if any tabs
+int irState = 0;
+bool detected = false;
 
 void setup() {
-  // initialize BLDC
-  //ESC.attach(8,1000,2000);
-  // attaches the servo on pin 6 to the servo object (braking) 
-  servo10.attach(10);
-  servoFunc2();
   pinMode(pinI1,OUTPUT); //set to output
   pinMode(pinI2,OUTPUT);
   pinMode(speedpinA,OUTPUT);
@@ -51,16 +34,15 @@ void setup() {
   IR.Init(IR_Receiver);
   Serial.begin(9600);
   Serial.println("init over");
-  // hall sensor
-  attachInterrupt(digitalPinToInterrupt(hallPin), hallInterrupt, FALLING);
+  
   // delay
   delay(1000);
-  
+  // hall sensor
+  attachInterrupt(digitalPinToInterrupt(hallPin), hallInterrupt, CHANGE);
 }
 
 void loop() {
 
-  //int power = robot.power;
   if(IR.IsDta())
   {
     IR.Recv(dta);
@@ -75,38 +57,25 @@ void loop() {
     {
       goCmd = false;
     }
-    if(dta[8] == 168)
-    {
-      servoFunc();
-    }
-    if(dta[8] == 136)
-    {
-      servoFunc2();
-    }
   }
-  //robot.power = (goCmd) ? 12 : 0;
+  if (detected == true)
+  {
+    delay(250);
+    motorSpeed(0);
+    detected = false;
+    delay(3000);
+    digitalWrite(ledPin,LOW);
+    motorSpeed(255);  
+  }
   (goCmd) ? motorSpeed(255) : motorSpeed(0);
-  //if(power != robot.power) {bldcPower();}  
 }
-
-/*void bldcPower() {
-  int i = 0;
-  i = map(robot.power, 0, 100, 1000, 2000);
-  // error handling
-  if(i < 1000 || i > 2000) {i = 1000;}
-  ESC.writeMicroseconds(i);
-  return;
-}*/
 
 void hallInterrupt() {
-  digitalWrite(ledPin,HIGH);
-  attachInterrupt(digitalPinToInterrupt(hallPin), ledFunc, RISING);
-  return;
-}
-
-void ledFunc() {
-  digitalWrite(ledPin,LOW);
-  return;
+  if( digitalRead(hallPin) == LOW){
+    digitalWrite(ledPin,HIGH);
+    detected = true;
+    detachInterrupt(digitalPinToInterrupt(hallPin));
+  }
 }
 
 void motorSpeed(int val)
@@ -117,13 +86,6 @@ void motorSpeed(int val)
   return;
 }
 
-void servoFunc() {
-  servo10.write(120);
-}
-
-void servoFunc2() {
-  servo10.write(10);
-}
 /*bool checkIRstates() {
   // line detected
   bool lined = false;
